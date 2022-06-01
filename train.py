a2e12b9cf35ea2e64683e7db6353b1addd6b4052
Amrit Ghimire <mail@amritghimire.com>
2022-06-01 14:55:44 +0545

    Add new MLEM Model

--- train.py
+++ train.py
@@ -12,12 +12,26 @@ def main():
     )
     rf.fit(data, y)
 
+    rf_2 = RandomForestClassifier(
+        n_jobs=2,
+        random_state=4,
+    )
+    rf_2.fit(data, y)
+
+
     save(
         rf,
         "rf",
         sample_data=data,
         description="Random Forest Classifier",
     )
+    save(
+        rf_2,
+        "rf_state_4",
+        sample_data=data,
+        description="Random Forest Classifier v2",
+    )
+
 
 
 if __name__ == "__main__":
